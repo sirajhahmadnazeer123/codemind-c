40db631dc1e91e5607c551380c91caf83e9a47b3
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-05-23 13:53:20 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>0;i--)     {         for(j=n;j>0;j--)         {             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>0;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>0;j--)
         {
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("
 ");
     }
-    return 0;
 }
